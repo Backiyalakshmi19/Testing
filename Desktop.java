19a2a0d5dd6ab81d09edd8f30f8a8c759e4aa695
Backiyalakshmi19 <105167969+Backiyalakshmi19@users.noreply.github.com>
2022-06-27 15:29:58 +0300

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,36 @@
+package org.system;
+
+public class Desktop extends Computer
+{
+
+	public void desktopSize(String  Desktop) {
+		System.out.println("desktopSize");
+	}
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		
+		Desktop dt=new Desktop();
+		dt.desktopSize("11.6 inch");
+		dt.ComputerModel("HP");
+		
+
+		
+	
+
+		
+	
+
+		
+	
+			
+		}
+
+		
+	}
+
+		
+		
+	
+
+

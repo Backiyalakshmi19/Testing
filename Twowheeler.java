caf1f3e8f084431ac690ad1307c3c4b1e4c6f861
Backiyalakshmi19 <105167969+Backiyalakshmi19@users.noreply.github.com>
2022-06-27 15:04:19 +0300

    Add files via upload

---
+++ Twowheeler.java
@@ -0,0 +1,26 @@
+package sel.pra;
+
+public class Twowheeler {
+
+	int noOfWheels=2;
+	short noOfMirrors=2;
+	long chassisNumber=1025634879L;
+	boolean Punctutred=true;
+	String bikeName="pulsar";
+	double runningKM=6528.22;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Twowheeler myTwoWheeler =new Twowheeler();
+
+		System.out.println(myTwoWheeler.noOfWheels);
+		System.out.println(myTwoWheeler.noOfMirrors);
+		System.out.println(myTwoWheeler.chassisNumber);
+		System.out.println(myTwoWheeler.Punctutred);	
+		System.out.println(myTwoWheeler.bikeName);	
+		System.out.println(myTwoWheeler. runningKM);	
+
+	}
+
+}
